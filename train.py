93ee3950f3a9e9125de4dbccd18b088903b46827
Sudarshan P <sudarshanpadekal2006@gmail.com>
2026-04-30 19:50:44 +0530

    Python file updated

--- train.py
+++ train.py
@@ -5,7 +5,8 @@ from torch.utils.data import DataLoader, random_split
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.Grayscale(num_output_channels= 3),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
 ])
 
 dataset = datasets.ImageFolder("dataset/", transform=transform)
@@ -15,16 +16,18 @@ val_size = len(dataset) - train_size
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle = True)
-val_loader = DataLoader(val_dataset, batch_size=32)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle = False)
 
 model = models.resnet18(weights = models.ResNet18_Weights.DEFAULT)
+for param in model.parameters():
+    param.requires_grad = False
 model.fc = torch.nn.Linear(model.fc.in_features, 2)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+optimizer = torch.optim.Adam(model.fc.parameters(), lr=0.001)
 
 epochs = 5
 
@@ -43,8 +46,7 @@ for epoch in range(epochs):
         optimizer.step()
         total_loss+=loss.item()
 
-    print(f"Epoch {epoch+1}, Loss: {total_loss:.4f}")
-
+    print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader):.4f}")
 model.eval()
 correct = 0
 total = 0
@@ -54,7 +56,7 @@ with torch.no_grad():
         images, labels = images.to(device), labels.to(device)
 
         outputs = model(images)
-        _, predicted = torch.max(outputs = 1)
+        _, predicted = torch.max(outputs, 1)
 
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
